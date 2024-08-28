327aa09f5d0571978e7de34c1058c8cc391e51cc
Vially Israel <viallyisrael+github@hotmail.com>
2024-08-28 15:00:49 -0300

    app node

---
+++ node/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const config = {
+    host: 'db', //container name
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+};
+const mysql = require('mysql')
+
+const connection = mysql.createConnection(config);
+
+
+const sql = `insert into people(name) values ('Israel')`;
+connection.query(sql);
+connection.end();
+
+app.get('/', (req,res)=> {
+    res.send('<h1>Full Cycle</h1>')
+})
+
+app.listen(port, ()=> {
+    console.log('Rodando na porta', port)
+})
